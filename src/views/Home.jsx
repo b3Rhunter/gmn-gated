600940dc66e64d2817288fba4305dac2ec57e0d5
Pub <96320073+b3Rhunter@users.noreply.github.com>
2022-06-20 15:11:48 -0500

    Add files via upload

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -29,9 +29,18 @@ function Home() {
 	  </p>
     </div>
     <i style={{fontSize: "5em"}}>🗞️</i>
-    </div>
     <div>
-      
+      <h1><b>How it Works</b></h1>
+      <h4 style={{textAlign: "justify"}}>Journalists from around the world submit their work to the publication. Every day, an editorial 
+	  vote is held. Subscribers, anyone who holds one of the publication’s NFTs, gather and vote for the day's story. 
+	  The journalist whose story is chosen is awarded in the organization's native token PAPER.</h4>
+<br></br>
+      <p style={{textAlign: "justify"}}><i>Once the votes are counted and the editors approve the copy for publication, the designers go 
+	  to work laying out the next day’s issue. Once complete, the design is deployed to the blockchain and our organization's 
+	  NFT updates with the latest story. For every issue of Good Morning News sold, the organization uses half of the proceeds 
+	  to buy back the PAPER token and the other half goes towards our PAPER/MATIC liquidity pool.</i></p>
+    <b style={{fontSize: "5em"}}>📰</b>
+    </div>
     </div>
 
 </div>);}
